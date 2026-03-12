8bad1886146afeb7b18ed979ba25a5627f753dc9
AKSHAY PATIL <akshaypatil9280@gmail.com>
2026-03-12 10:06:41 +0530

    Add users views : RegistrationView, ProfileView, UserView

--- EventBookingAPI/users/views.py
+++ EventBookingAPI/users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework import generics, mixins
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .models import User, Profile
+from .serializers import RegisterSerializer, ProfileSerializer, UserSerializer
 
 # Create your views here.
+class RegistrationView(mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset=User.objects.all()
+    serializer_class=RegisterSerializer
+    permission_classes=[AllowAny]
+
+    def post(self,request,*args,**kwargs):
+        return self.create(request,*args,**kwargs)
+    
+class ProfileSerializer(generics.RetrieveUpdateAPIView):
+    serializer_class=ProfileSerializer
+    permission_classes=[IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user.profile
+        
+class UserView(generics.GenericAPIView):
+    serializer_class=UserSerializer
+    permission_classes=[IsAuthenticated]
+
+    def get(self,request,*args,**kwargs):
+        user=request.user
+        serializer=self.get_serializer(user)
+        return Response(serializer.data,status=status.HTTP_200_OK)
